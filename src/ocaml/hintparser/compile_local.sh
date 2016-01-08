6aac192f408241c0c4dc3e676929cb93ff9ab0a1
shpark <sanghoon.park@sf.snu.ac.kr>
2016-01-09 02:09:39 +0900

    compile_local.sh in hintparser modify

--- src/ocaml/hintparser/compile_local.sh
+++ src/ocaml/hintparser/compile_local.sh
@@ -2,12 +2,7 @@
 atdgen -t hintParser.atd
 atdgen -j hintParser.atd
 
-ocamlfind ocamlc -c hintParser_t.mli -package atdgen
-ocamlfind ocamlc -c hintParser_j.mli -package atdgen
-ocamlfind ocamlopt -c hintParser_t.ml -package atdgen
-ocamlfind ocamlopt -c hintParser_j.ml -package atdgen
-#ocamlfind ocamlopt -c readhintParser.ml -package atdgen
-
-#ocamlfind ocamlopt -o readhintParser \
-#  hintParser_t.mli hintParser_t.ml hintParser_j.mli hintParser_j.ml \
-#  readhint.ml -package atdgen -linkpkg
+#ocamlfind ocamlc -c hintParser_t.mli -package atdgen
+#ocamlfind ocamlc -c hintParser_j.mli -package atdgen
+#ocamlfind ocamlopt -c hintParser_t.ml -package atdgen
+#ocamlfind ocamlopt -c hintParser_j.ml -package atdgen
--- src/ocaml/hintparser/rm_compiledfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm *.cmx
-rm *.cmi
-rm *.o
