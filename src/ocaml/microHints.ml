e3ea81b944423728570b59fa456fd7fd620e6bc3
markshin <shin.dongyeon@gmail.com>
2016-02-04 15:15:17 +0900

    Add appliers of subShl & mulBool

--- src/ocaml/microHints.ml
+++ src/ocaml/microHints.ml
@@ -104,10 +104,13 @@ let propagate_micro
       SubConstAddApplier.apply options args
   | CoreHint_t.SubSdiv (options:CoreHint_t.sub_sdiv) ->
       SubSdivApplier.apply options args
+  | CoreHint_t.SubShl (options:CoreHint_t.sub_shl) ->
+      SubShlApplier.apply options args
+  | CoreHint_t.MulBool (options:CoreHint_t.mul_bool) ->
+      MulBoolApplier.apply options args
   | CoreHint_t.MulMone (options:CoreHint_t.mul_mone) ->
       MulMoneApplier.apply options args
   | CoreHint_t.MulNeg (options:CoreHint_t.mul_neg) ->
       MulNegApplier.apply options args
-
-
+  
   (* NOTE: Add here to add a new rule *)
