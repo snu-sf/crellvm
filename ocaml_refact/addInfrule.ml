edb01267977b679ce1e50522959952ec44c55da3
Sung-HwanLee <hwan.lee67@gmail.com>
2016-03-02 18:50:21 +0900

    Fix types of add_infrule

--- ocaml_refact/addInfrule.ml
+++ ocaml_refact/addInfrule.ml
@@ -18,57 +18,49 @@ type atom = AtomImpl.atom
 
 (* add an inference rule for a phinode *)
 let add_infrule_phinode (coq_infrule:Infrule.t)
-                        (pos_phinode:CoreHint_t.position_phinode)
+                        (prev_block:atom)
                         (block_hint:ValidationHint.stmts) : ValidationHint.stmts =
-  let prev_block = pos_phinode.prev_block_name in
   let hint_phinodes =
     let infrules =
       match Alist.lookupAL block_hint.phinodes prev_block with
       | None -> failwith "add_infrule_phinode: no previous block exists"
       | Some infrules -> infrules
     in
-    (* TODO: append doesn't work, maybe because 'infrules' is not a list, but an alist *)
-    (* let infrules = List.append infrules [coq_infrule] in *)
+    let infrules = List.append infrules [coq_infrule] in
     Alist.updateAL block_hint.phinodes prev_block infrules in
   let block_hint = { block_hint with phinodes = hint_phinodes } in
   block_hint
 
 (* add an inference rule for a command *)
 let add_infrule_command (infrule:Infrule.t)
-                        (pos_command:CoreHint_t.position_command)
                         (line_num:int)
                         (block_hint:ValidationHint.stmts)
     : ValidationHint.stmts =
   let hint_cmds =
     List.mapi
       (fun n -> fun (infrules, inv) ->
-                if not (line_num = n) then infrules
-                else infrules)
-      (* TODO: fix this *)
-      (* else (List.append infrules [coq_infrule], inv)) *)
-      block_hint.cmds in
-  (* TODO: fix hint_cmds is not well-typed *)
-  (* let block_hint = { block_hint with cmds = hint_cmds } in *)
+                if not (line_num = n) then (infrules, inv)
+                else (List.append infrules [infrule], inv))
+      block_hint.cmds
+  in
+  let block_hint = { block_hint with cmds = hint_cmds } in
   block_hint
 
 (* add an inference rule at the "at" in the hint. *)
 let add_infrule (pos:Position.t)
                 (infrule:Infrule.t)
-                (hint:ValidationHint.fdef)
+                (block_hints:ValidationHint.fdef)
     : ValidationHint.fdef =
-  let block_hints = hint in
-  failwith "TODO: pos is not well typed"
-           (* let pos = infrule.position in *)
-           (* let block_name = Position.get_block_name_from_position pos lfd rfd in *)
-           (* match Alist.lookupAL block_hints block_name with *)
-           (* | None -> failwith "add_infrule: no such block hint" *)
-           (* | Some block_hint -> *)
-           (*     let block_hint_new = *)
-           (*       (match pos with *)
-           (*       | CoreHint_t.Phinode phinode -> *)
-           (*           add_infrule_phinode infrule phinode block_hint *)
-           (*       | CoreHint_t.Command command -> *)
-           (*           let (_, line_num) = Position.get_pos_from_command command lfd rfd in *)
-           (*           add_infrule_command infrule command line_num block_hint) in *)
-           (*     let block_hints = Alist.updateAL block_hints block_name block_hint_new in *)
-           (*     block_hints *)
+  let (block_name, idx) = pos in
+  match Alist.lookupAL block_hints block_name with
+  | None -> failwith "add_infrule: no such block hint"
+  | Some block_hint ->
+      let block_hint_new =
+        (match idx with
+        | Position.Phinode prev_block ->
+            add_infrule_phinode infrule prev_block block_hint
+        | Position.Command line_num ->
+            add_infrule_command infrule line_num block_hint)
+      in
+      let block_hints = Alist.updateAL block_hints block_name block_hint_new in
+      block_hints
