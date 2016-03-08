1d1dc1968afbf283d89483b087b0caba8d35fde9
Sung-HwanLee <hwan.lee67@gmail.com>
2016-03-08 14:09:10 +0900

    Fix some bugs

--- coq/exec/Infrules.v
+++ coq/exec/Infrules.v
@@ -8,6 +8,8 @@ Require Import Metatheory.
 Import LLVMsyntax.
 Import LLVMinfra.
 
+Require Import Integers.
+
 Require Import Exprs.
 Require Import Hints.
 Require Import TODO.
@@ -24,8 +26,8 @@ Definition cond_plus (s:sz) (c1 c2 c3: INTEGER.t) : bool :=
 Notation "$$ inv |- y =r1 rhs $$" := (Invariant.in_src y rhs inv) (at level 41).
 Notation "$$ inv |- y =r2 rhs $$" := (Invariant.in_tgt y rhs inv) (at level 41).
 
-Notation "{{ h +++ y =r1 rhs }}" := (add_eq_reg_orig (y, rhs) h) (at level 41).
-Notation "{{ h +++ y =r2 rhs }}" := (add_eq_reg_opt (y, rhs) h) (at level 41).
+Notation "{{ inv +++ y =r1 rhs }}" := (Invariant.add_to_src y rhs inv) (at level 41).
+Notation "{{ inv +++ y =r2 rhs }}" := (Invariant.add_to_tgt y rhs inv) (at level 41).
 
 (* TODO *)
 Definition apply_infrule
@@ -33,12 +35,16 @@ Definition apply_infrule
            (inv0:Invariant.t): Invariant.t :=
   match infrule with
 
-  | rule_add_associative z y x s c1 c2 c3 =>
-    if $$ inv0 |- (Expr.value (ValueT.id y)) =r1 (Expr.bop bop_add s (Expr.value (ValueT.id x)) (ValueT.const (const_int s c1))) $$ &&
-       $$ inv0 |- (Expr.value (ValueT.id z)) =r1 (Expr.bop bop_add s (Expr.value (ValueT.id y)) (ValueT.const (const_int s c2))) $$ &&
+  | Infrule.add_associative x y z c1 c2 c3 s =>
+    if $$ inv0 |- (Expr.value (ValueT.id y)) =r1 (Expr.bop bop_add s (ValueT.id x) (ValueT.const (const_int s c1))) $$ &&
+       $$ inv0 |- (Expr.value (ValueT.id z)) =r1 (Expr.bop bop_add s (ValueT.id y) (ValueT.const (const_int s c2))) $$ &&
        cond_plus s c1 c2 c3
-    then {{inv0 +++ (Expr.vlue (ValueT.id z)) =r1 (Expr.bop bop_add s (Expr.value (ValueT.id x)) (ValueT.const (const_int s c3)))}}
+    then {{inv0 +++ (Expr.value (ValueT.id z)) =r1 (Expr.bop bop_add s (ValueT.id x) (ValueT.const (const_int s c3)))}}
     else inv0
+
+  | _ => inv0
+
+  end.
 
 (* TODO *)
 Definition apply_infrules
