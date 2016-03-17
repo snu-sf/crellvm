94736a181cac3eba0d9d82700a2296c0e24b5237
alxest <youngju.song@sf.snu.ac.kr>
2016-03-17 18:12:30 +0900

    Fill TODO.map for extraction
    Duplicate same definition can lead to seperation of theorems.

--- coq/exec/TODO.v
+++ coq/exec/TODO.v
@@ -83,6 +83,8 @@ Fixpoint unique
   | a::l => negb (in_dec eq_dec a l) && unique eq_dec l
   end.
 
+Definition mapAL := Metatheory.EnvImpl.map.
+
 Definition mapiAL A B (f: atom -> A -> B) (l:AssocList A): AssocList B :=
   List.map
     (fun (p:atom * A) =>
