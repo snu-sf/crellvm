d2d5210344a51a1868e9c36660f69db164db3146
Juneyoung Lee <aqjune@gmail.com>
2016-03-24 02:30:54 +0900

    Add Inference rules for Load-load optimization

--- coq/exec/Infrules.v
+++ coq/exec/Infrules.v
@@ -31,6 +31,21 @@ Definition signbit_of (s:sz) : option Int :=
     | S n => Some (Zneg (power_sz n))
   end.
 
+(* Copied from validator/syntax_ext.v because ocaml-extracted version of this code cannot find validator/syntax_ext.v *)
+
+Fixpoint collect_global_ids (ps: products) :=
+  match ps with
+  | nil => nil
+  | (product_gvar (gvar_intro i _ _ _ _ _))::tps => (i::(collect_global_ids tps))
+  | (product_gvar (gvar_external i _ _))::tps => (i::(collect_global_ids tps))
+  | _::tps => collect_global_ids tps
+  end.
+
+Definition cond_is_global (x:IdT.t) (m:module) : bool :=
+  match m with
+  | module_intro _ _ ps => 
+    List.existsb (fun y => IdT.eq_dec (Tag.physical, y) x) (collect_global_ids ps)
+  end.
 
 Definition cond_plus (s:sz) (c1 c2 c3: INTEGER.t) : bool :=
   (Int.eq_dec _)
@@ -47,13 +62,108 @@ Definition cond_signbit (s:sz) (v:ValueT.t) : bool :=
   | _, _ => false
   end.
 
+
+(* cond_replace_value_lessdef x y v v' : Given x >= y', if all x in v1 are replaced into y(let the replaced value v2), is v2 >= v'? *)
+Definition cond_replace_lessdef_value (x:IdT.t) (y:ValueT.t) (v v':ValueT.t) : bool :=
+  match v, v' with
+  | ValueT.id a, _ =>
+    (IdT.eq_dec a x && ValueT.eq_dec v' y) || (ValueT.eq_dec v v')
+  | ValueT.const c1, ValueT.const c2 => const_dec c1 c2 (* How about the case, c1 == undef? *)
+  | _,_ => false
+  end.
+
+(* cond_replace_value_lessdef x y v v' : Given x >= y', if all x in v1 are replaced into y(let the replaced value v2), is v2 >= v'? *)
+Fixpoint cond_replace_lessdef_valuelist (x:IdT.t) (y:ValueT.t) (lsv1 lsv2:list (sz * ValueT.t)) : bool :=
+  match lsv1 , lsv2 with
+  | nil, nil => true
+  | (_,h1)::t1,(_,h2)::t2 =>
+    cond_replace_lessdef_value x y h1 h2 && cond_replace_lessdef_valuelist x y t1 t2
+  | _,_ => false
+  end.
+
+(* cond_replace_lessdef x y e e' : If all x in e are replaced into y(let the replaced expression e2), is e2 >= e'? *)
+Definition cond_replace_lessdef (x:IdT.t) (y:ValueT.t) (e e':Expr.t) : bool :=
+  match e, e' with
+  | Expr.bop b1 s1 v1 w1, Expr.bop b2 s2 v2 w2 =>
+    bop_dec b1 b2 &&
+    Size.dec s1 s2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    cond_replace_lessdef_value x y w1 w2
+  | Expr.fbop fb1 fp1 v1 w1, Expr.fbop fb2 fp2 v2 w2 =>
+    fbop_dec fb1 fb2 &&
+    floating_point_dec fp1 fp2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    cond_replace_lessdef_value x y w1 w2
+  | Expr.extractvalue t1 v1 lc1 u1, Expr.extractvalue t2 v2 lc2 u2 =>
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    list_const_dec lc1 lc2 &&
+    typ_dec u1 u2
+  | Expr.insertvalue t1 v1 u1 w1 lc1, Expr.insertvalue t2 v2 u2 w2 lc2 =>
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    typ_dec u1 u2 &&
+    cond_replace_lessdef_value x y w1 w2 &&
+    list_const_dec lc1 lc2
+  | Expr.gep ib1 t1 v1 lsv1 u1, Expr.gep ib2 t2 v2 lsv2 u2 =>
+    inbounds_dec ib1 ib2 &&
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    cond_replace_lessdef_valuelist x y lsv1 lsv2 &&
+    typ_dec u1 u2
+  | Expr.trunc top1 t1 v1 u1, Expr.trunc top2 t2 v2 u2 =>
+    truncop_dec top1 top2 &&
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    typ_dec u1 u2
+  | Expr.ext ex1 t1 v1 u1, Expr.ext ex2 t2 v2 u2 =>
+    extop_dec ex1 ex2 &&
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    typ_dec u1 u2
+  | Expr.cast cop1 t1 v1 u1, Expr.cast cop2 t2 v2 u2 =>
+    castop_dec cop1 cop2 &&
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    typ_dec u1 u2
+  | Expr.icmp con1 t1 v1 w1, Expr.icmp con2 t2 v2 w2 =>
+    cond_dec con1 con2 &&
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    cond_replace_lessdef_value x y w1 w2
+  | Expr.fcmp fcon1 fp1 v1 w1, Expr.fcmp fcon2 fp2 v2 w2 =>
+    fcond_dec fcon1 fcon2 &&
+    floating_point_dec fp1 fp2 &&
+    cond_replace_lessdef_value x y v1 v2 &&
+    cond_replace_lessdef_value x y w1 w2
+  | Expr.select v1 t1 w1 z1, Expr.select v2 t2 w2 z2 =>
+    cond_replace_lessdef_value x y v1 v2 &&
+    typ_dec t1 t2 &&
+    cond_replace_lessdef_value x y w1 w2 &&
+    cond_replace_lessdef_value x y z1 z2
+  | Expr.value v1, Expr.value v2 =>
+    cond_replace_lessdef_value x y v1 v2
+  | Expr.load v1 t1 a1, Expr.load v2 t2 a2 => 
+    (* FIXME : Is this condition ok? *)
+    cond_replace_lessdef_value x y v1 v2 &&
+    typ_dec t1 t2 &&
+    Align.dec a1 a2
+  | _, _ => false
+  end.
+
+
 Notation "$$ inv |- y >=src rhs $$" := (ExprPairSet.mem (y, rhs) inv.(Invariant.src).(Invariant.lessdef)) (at level 41).
 Notation "$$ inv |- y >=tgt rhs $$" := (ExprPairSet.mem (y, rhs) inv.(Invariant.tgt).(Invariant.lessdef)) (at level 41).
+Notation "$$ inv |-allocasrc y $$" := (IdTSet.mem y inv.(Invariant.src).(Invariant.allocas)) (at level 41).
+Notation "$$ inv |-allocatgt y $$" := (IdTSet.mem y inv.(Invariant.tgt).(Invariant.allocas)) (at level 41).
 Notation "{{ inv +++ y >=src rhs }}" := (Invariant.update_src (Invariant.update_lessdef (ExprPairSet.add (y, rhs))) inv) (at level 41).
 Notation "{{ inv +++ y >=tgt rhs }}" := (Invariant.update_tgt (Invariant.update_lessdef (ExprPairSet.add (y, rhs))) inv) (at level 41).
+Notation "{{ inv +++ y _|_src x }}" := (Invariant.update_src (Invariant.update_noalias (ValueTPairSet.add (y, x))) inv) (at level 41).
+Notation "{{ inv +++ y _|_tgt x }}" := (Invariant.update_tgt (Invariant.update_noalias (ValueTPairSet.add (y, x))) inv) (at level 41).
 
 (* TODO *)
 Definition apply_infrule
+           (m_src m_tgt:module)
            (infrule:Infrule.t)
            (inv0:Invariant.t): Invariant.t :=
   match infrule with
@@ -90,11 +200,38 @@ Definition apply_infrule
     if $$ inv0 |- (Expr.value (ValueT.id z)) >=src (Expr.bop bop_mul Size.One (ValueT.id x) (ValueT.id y)) $$
     then {{inv0 +++ (Expr.value (ValueT.id z)) >=src (Expr.bop bop_and Size.One (ValueT.id x) (ValueT.id y)) }}
     else inv0
+  | Infrule.transitivity e1 e2 e3 =>
+    if $$ inv0 |- e1 >=src e2 $$ &&
+       $$ inv0 |- e2 >=src e3 $$ 
+    then {{inv0 +++ e1 >=src e3}}
+    else inv0
+  | Infrule.noalias_global_alloc x y =>
+    if $$ inv0 |-allocasrc y $$ &&
+       cond_is_global x m_src
+    then {{inv0 +++ (ValueT.id y) _|_src (ValueT.id x) }} (* FIXME : is there no distinction between local and global variables? *)
+    else inv0
+  | Infrule.transitivity_pointer_lhs p q v ty a =>
+    if $$ inv0 |- (Expr.value p) >=src (Expr.value q) $$ &&
+       $$ inv0 |- (Expr.load q ty a) >=src (Expr.value v) $$
+    then {{inv0 +++ (Expr.load p ty a) >=src (Expr.value v)}}
+    else inv0
+  | Infrule.transitivity_pointer_rhs p q v ty a =>
+    if $$ inv0 |- (Expr.value p) >=src (Expr.value q) $$ &&
+       $$ inv0 |- (Expr.value v) >=src (Expr.load p ty a) $$
+    then {{inv0 +++ (Expr.value v) >=src (Expr.load q ty a)}}
+    else inv0
+  | Infrule.replace_rhs x y e1 e2 e2' =>
+    if $$ inv0 |- (Expr.value (ValueT.id x)) >=src (Expr.value y) $$ &&
+       $$ inv0 |- e1 >=src e2 $$ &&
+       cond_replace_lessdef x y e2 e2'
+    then {{inv0 +++ e1 >=src e2'}}
+    else inv0
   | _ => inv0 (* TODO *)
   end.
 
 (* TODO *)
 Definition apply_infrules
+           (m_src m_tgt:module)
            (infrules:list Infrule.t)
            (inv0:Invariant.t): Invariant.t :=
-  fold_left (fun i r => apply_infrule r i) infrules inv0.
+  fold_left (fun i r => apply_infrule m_src m_tgt r i) infrules inv0.
--- coq/exec/Validator.v
+++ coq/exec/Validator.v
@@ -36,6 +36,7 @@ Definition debug_print_cmd_pair (c_src c_tgt:cmd): cmd * cmd :=
   (c_src, c_tgt).
 
 Fixpoint valid_cmds
+         (m_src m_tgt:module)
          (src tgt:list cmd)
          (hint:list (list Infrule.t * Invariant.t))
          (inv0:Invariant.t): option Invariant.t :=
@@ -48,11 +49,11 @@ Fixpoint valid_cmds
     match postcond_cmd cmd_src cmd_tgt inv0 with
     | None => failwith_None "valid_cmds: postcond_cmd returned None" nil
     | Some inv1 =>
-      let inv2 := apply_infrules infrules inv1 in
+      let inv2 := apply_infrules m_src m_tgt infrules inv1 in
       let inv3 := reduce_maydiff inv2 in
       let inv := debug_print_validation_process infrules inv0 inv1 inv2 inv3 inv in
       if Invariant.implies inv3 inv
-      then valid_cmds src tgt hint inv
+      then valid_cmds m_src m_tgt src tgt hint inv
       else failwith_None "valid_cmds: Invariant.implies returned false" nil
     end
   | nil, nil, nil => Some inv0
@@ -62,6 +63,7 @@ Fixpoint valid_cmds
 Definition valid_phinodes
            (hint_fdef:ValidationHint.fdef)
            (inv0:Invariant.t)
+           (m_src m_tgt:module)
            (blocks_src blocks_tgt:blocks)
            (l_from l_to:l): bool :=
   let l_from := (debug_print atom_printer l_from) in
@@ -75,7 +77,7 @@ Definition valid_phinodes
     match postcond_phinodes l_from phinodes_src phinodes_tgt inv0 with
       | None => failwith_false "valid_phinodes: postcond_phinodes returned None at phinode" (l_from::l_to::nil)
       | Some inv1 =>
-        let inv2 := apply_infrules infrules inv1 in
+        let inv2 := apply_infrules m_src m_tgt infrules inv1 in
         let inv3 := reduce_maydiff inv2 in
         let inv := hint_stmts.(ValidationHint.invariant_after_phinodes) in
         let inv := debug_print_validation_process infrules inv0 inv1 inv2 inv3 inv in
@@ -89,6 +91,7 @@ Definition valid_phinodes
 Definition valid_terminator
            (hint_fdef:ValidationHint.fdef)
            (inv0:Invariant.t)
+           (m_src m_tgt:module)
            (blocks_src blocks_tgt:blocks)
            (bid:l)
            (src tgt:terminator): bool :=
@@ -118,10 +121,10 @@ Definition valid_terminator
     if negb (l_dec l2_src l2_tgt)
     then failwith_false "valid_terminator: labels of false branches not matched at block" [bid]
     else
-    if negb (valid_phinodes hint_fdef inv0 blocks_src blocks_tgt bid l1_src)
+    if negb (valid_phinodes hint_fdef inv0 m_src m_tgt blocks_src blocks_tgt bid l1_src)
     then failwith_false "valid_terminator: valid_phinodes of true branches failed at block" [bid]
     else
-    if negb (valid_phinodes hint_fdef inv0 blocks_src blocks_tgt bid l2_src)
+    if negb (valid_phinodes hint_fdef inv0 m_src m_tgt blocks_src blocks_tgt bid l2_src)
     then failwith_false "valid_terminator: valid_phinodes of false branches failed at block" [bid]
     else true
     
@@ -129,7 +132,7 @@ Definition valid_terminator
     if negb (l_dec l_src l_tgt)
     then failwith_false "valid_terminator: labels of unconditional branches not matched at block" [bid]
     else
-    if negb (valid_phinodes hint_fdef inv0 blocks_src blocks_tgt bid l_src)
+    if negb (valid_phinodes hint_fdef inv0 m_src m_tgt blocks_src blocks_tgt bid l_src)
     then failwith_false "valid_terminator: valid_phinodes of unconditional branches failed at block" [bid]
     else true
     
@@ -140,14 +143,15 @@ Definition valid_terminator
 Definition valid_stmts
            (hint_fdef:ValidationHint.fdef)
            (hint:ValidationHint.stmts)
+           (m_src m_tgt:module)
            (blocks_src blocks_tgt:blocks)
            (bid:l) (src tgt:stmts): bool :=
   let '(stmts_intro phinodes_src cmds_src terminator_src) := src in
   let '(stmts_intro phinodes_tgt cmds_tgt terminator_tgt) := tgt in
-  match valid_cmds cmds_src cmds_tgt hint.(ValidationHint.cmds) hint.(ValidationHint.invariant_after_phinodes) with
+  match valid_cmds m_src m_tgt cmds_src cmds_tgt hint.(ValidationHint.cmds) hint.(ValidationHint.invariant_after_phinodes) with
   | None => failwith_false "valid_stmts: valid_cmds failed at block" [bid]
   | Some inv =>
-    if negb (valid_terminator hint_fdef inv blocks_src blocks_tgt bid terminator_src terminator_tgt)
+    if negb (valid_terminator hint_fdef inv m_src m_tgt blocks_src blocks_tgt bid terminator_src terminator_tgt)
     then failwith_false "valid_stmts: valid_terminator failed at block" [bid]
     else true              
   end.
@@ -170,6 +174,7 @@ Definition valid_entry_stmts (src tgt:stmts) (hint:ValidationHint.stmts): bool :
   .
 
 Definition valid_fdef
+           (m_src m_tgt:module)
            (src tgt:fdef)
            (hint:ValidationHint.fdef): bool :=
   let '(fdef_intro fheader_src blocks_src) := src in
@@ -201,7 +206,7 @@ Definition valid_fdef
     (fun bid stmts_src stmts_tgt =>
        match lookupAL _ hint bid with
        | Some hint_stmts =>
-         if negb (valid_stmts hint hint_stmts blocks_src blocks_tgt bid stmts_src stmts_tgt)
+         if negb (valid_stmts hint hint_stmts m_src m_tgt blocks_src blocks_tgt bid stmts_src stmts_tgt)
          then failwith_false "valid_fdef: valid_stmts failed at block" (fid_src::bid::nil)
          else true
 
@@ -209,7 +214,7 @@ Definition valid_fdef
        end)
     blocks_src blocks_tgt.
 
-Definition valid_product (hint:ValidationHint.products) (src tgt:product): bool :=
+Definition valid_product (hint:ValidationHint.products) (m_src m_tgt:module) (src tgt:product): bool :=
   match src, tgt with
   | product_gvar gvar_src, product_gvar gvar_tgt =>
     if negb (gvar_dec gvar_src gvar_tgt)
@@ -228,7 +233,7 @@ Definition valid_product (hint:ValidationHint.products) (src tgt:product): bool 
     match lookupAL _ hint fid_src with
     | None => failwith_false "valid_product: hint of function not exist" [fid_src]
     | Some hint_fdef =>
-      if negb (valid_fdef fdef_src fdef_tgt hint_fdef)
+      if negb (valid_fdef m_src m_tgt fdef_src fdef_tgt hint_fdef)
       then failwith_false "valid_product: valid_fdef failed" [fid_src]
       else true  
     end
@@ -236,8 +241,8 @@ Definition valid_product (hint:ValidationHint.products) (src tgt:product): bool 
     failwith_false "valid_product: source and target product types not matched" nil
   end.
 
-Definition valid_products (hint:ValidationHint.products) (src tgt:products): bool :=
-  list_forallb2 (valid_product hint) src tgt.
+Definition valid_products (hint:ValidationHint.products) (m_src m_tgt:module) (src tgt:products): bool :=
+  list_forallb2 (valid_product hint m_src m_tgt) src tgt.
 
 Definition valid_module (hint:ValidationHint.module) (src tgt:module): bool :=
   let '(module_intro layouts_src namedts_src products_src) := src in
@@ -248,7 +253,7 @@ Definition valid_module (hint:ValidationHint.module) (src tgt:module): bool :=
   if negb (namedts_dec namedts_src namedts_tgt)
   then failwith_false "valid_module: named types not matched" nil
   else
-  if negb (valid_products hint products_src products_tgt)
+  if negb (valid_products hint src tgt products_src products_tgt)
   then failwith_false "valid_module: valid_products failed" nil
   else true.
 
