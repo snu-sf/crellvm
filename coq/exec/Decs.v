16e025947a0985fe69f96dd5a0df36842bb6b777
Yoonseung Kim <yoonseung.kim@sf.snu.ac.kr>
2016-05-24 14:59:03 +0900

    Change name my_const_dec to const_eqb, my_gvar_dec to gvar_eqb

--- coq/exec/Decs.v
+++ coq/exec/Decs.v
@@ -69,7 +69,7 @@ Hint Resolve sz_dec: EqDecDb.
 
 (** * constant_dec *)
 
-Fixpoint my_const_dec (c1 c2:const) : bool :=
+Fixpoint const_eqb (c1 c2:const) : bool :=
   match c1,c2 with
     | const_zeroinitializer t1, 
       const_zeroinitializer t2 => typ_dec t1 t2
@@ -87,59 +87,59 @@ Fixpoint my_const_dec (c1 c2:const) : bool :=
       const_struct t2 lc2 => typ_dec t1 t2 && 
       (fix f (lc1 lc2: list const) :=
         match lc1, lc2 with
-          | cons hd1 tl1, cons hd2 tl2 => my_const_dec hd1 hd2 && f tl1 tl2
+          | cons hd1 tl1, cons hd2 tl2 => const_eqb hd1 hd2 && f tl1 tl2
           | nil, nil => true
           | _, _ => false
         end) lc1 lc2
     | const_gid t1 x1,
       const_gid t2 x2 => typ_dec t1 t2 && id_dec x1 x2
     | const_truncop top1 cc1 t1,
-      const_truncop top2 cc2 t2 => truncop_dec top1 top2 && my_const_dec cc1 cc2 && typ_dec t1 t2
+      const_truncop top2 cc2 t2 => truncop_dec top1 top2 && const_eqb cc1 cc2 && typ_dec t1 t2
     | const_extop eop1 cc1 t1,
-      const_extop eop2 cc2 t2 => extop_dec eop1 eop2 && my_const_dec cc1 cc2 && typ_dec t1 t2
+      const_extop eop2 cc2 t2 => extop_dec eop1 eop2 && const_eqb cc1 cc2 && typ_dec t1 t2
     | const_castop cop1 cc1 t1,
-      const_castop cop2 cc2 t2 => castop_dec cop1 cop2 && my_const_dec cc1 cc2 && typ_dec t1 t2
+      const_castop cop2 cc2 t2 => castop_dec cop1 cop2 && const_eqb cc1 cc2 && typ_dec t1 t2
     | const_gep ib1 cc1 lc1, 
-      const_gep ib2 cc2 lc2 => inbounds_dec ib1 ib2 && my_const_dec cc1 cc2 &&      
+      const_gep ib2 cc2 lc2 => inbounds_dec ib1 ib2 && const_eqb cc1 cc2 &&      
       (fix f (lc1 lc2: list const) :=
         match lc1, lc2 with
-          | cons hd1 tl1, cons hd2 tl2 => my_const_dec hd1 hd2 && f tl1 tl2
+          | cons hd1 tl1, cons hd2 tl2 => const_eqb hd1 hd2 && f tl1 tl2
           | nil, nil => true
           | _, _ => false
         end) lc1 lc2
     | const_select cc1 cd1 ce1,
-      const_select cc2 cd2 ce2 => my_const_dec cc1 cc2 && my_const_dec cd1 cd2 && my_const_dec ce1 ce2
+      const_select cc2 cd2 ce2 => const_eqb cc1 cc2 && const_eqb cd1 cd2 && const_eqb ce1 ce2
     | const_icmp cd1 ce1 cf1,
-      const_icmp cd2 ce2 cf2 => cond_dec cd1 cd2 && my_const_dec ce1 ce2 && my_const_dec cf1 cf2
+      const_icmp cd2 ce2 cf2 => cond_dec cd1 cd2 && const_eqb ce1 ce2 && const_eqb cf1 cf2
     | const_fcmp cd1 ce1 cf1,
-      const_fcmp cd2 ce2 cf2 => fcond_dec cd1 cd2 && my_const_dec ce1 ce2 && my_const_dec cf1 cf2
+      const_fcmp cd2 ce2 cf2 => fcond_dec cd1 cd2 && const_eqb ce1 ce2 && const_eqb cf1 cf2
     | const_extractvalue cc1 lc1,
-      const_extractvalue cc2 lc2 => my_const_dec cc1 cc2 && 
+      const_extractvalue cc2 lc2 => const_eqb cc1 cc2 && 
       (fix f (lc1 lc2: list const) :=
         match lc1, lc2 with
-          | cons hd1 tl1, cons hd2 tl2 => my_const_dec hd1 hd2 && f tl1 tl2
+          | cons hd1 tl1, cons hd2 tl2 => const_eqb hd1 hd2 && f tl1 tl2
           | nil, nil => true
           | _, _ => false
         end) lc1 lc2
     | const_insertvalue cc1 cd1 lc1,
-      const_insertvalue cc2 cd2 lc2 => my_const_dec cc1 cc2 && my_const_dec cd1 cd2 && 
+      const_insertvalue cc2 cd2 lc2 => const_eqb cc1 cc2 && const_eqb cd1 cd2 && 
       (fix f (lc1 lc2: list const) :=
         match lc1, lc2 with
-          | cons hd1 tl1, cons hd2 tl2 => my_const_dec hd1 hd2 && f tl1 tl2
+          | cons hd1 tl1, cons hd2 tl2 => const_eqb hd1 hd2 && f tl1 tl2
           | nil, nil => true
           | _, _ => false
         end) lc1 lc2
     | const_bop b1 cc1 cd1,
-      const_bop b2 cc2 cd2 => bop_dec b1 b2 && my_const_dec cc1 cc2 && my_const_dec cd1 cd2
+      const_bop b2 cc2 cd2 => bop_dec b1 b2 && const_eqb cc1 cc2 && const_eqb cd1 cd2
     | const_fbop b1 cc1 cd1,
-      const_fbop b2 cc2 cd2 => fbop_dec b1 b2 && my_const_dec cc1 cc2 && my_const_dec cd1 cd2
+      const_fbop b2 cc2 cd2 => fbop_dec b1 b2 && const_eqb cc1 cc2 && const_eqb cd1 cd2
     | _,_ => false
   end.
 
-Definition my_gvar_dec (g1 g2:gvar) : bool :=
+Definition gvar_eqb (g1 g2:gvar) : bool :=
   match g1,g2 with
     | gvar_intro x1 lk1 gs1 t1 c1 a1,
-      gvar_intro x2 lk2 gs2 t2 c2 a2 => id_dec x1 x2 && linkage_dec lk1 lk2 && gvar_spec_dec gs1 gs2 && typ_dec t1 t2 && my_const_dec c1 c2 && align_dec a1 a2
+      gvar_intro x2 lk2 gs2 t2 c2 a2 => id_dec x1 x2 && linkage_dec lk1 lk2 && gvar_spec_dec gs1 gs2 && typ_dec t1 t2 && const_eqb c1 c2 && align_dec a1 a2
     | gvar_external x1 gs1 t1,
       gvar_external x2 gs2 t2 => id_dec x1 x2 && gvar_spec_dec gs1 gs2 && typ_dec t1 t2 
     | _,_ => false
