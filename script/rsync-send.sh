7a81872945f6c3e1f9d326e7d62713f8d31c5b29
alxest <youngju.song@sf.snu.ac.kr>
2016-07-30 21:17:26 +0900

    Fix typo

--- script/rsync-send.sh
+++ script/rsync-send.sh
@@ -3,7 +3,7 @@ PROOF_BUILD_DIR=.proof_build
 
 rsync -av --delete --prune-empty-dirs \
     --exclude "$PROOF_BUILD_DIR" \
-    --exclude "lib-llvm/" --exclude "ocaml" \
+    --exclude "lib/llvm/" --exclude "ocaml" \
     --include '*/' \
     --include '*.v' --include 'Makefile' \
     --include '*.ott' --include 'fixextract.py' \
