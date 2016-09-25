c53d35ae807dc84a7703045d7eee3511618aa79c
Yoonseung Kim <yoonseung.kim@sf.snu.ac.kr>
2016-09-25 19:52:55 +0900

    Fix spec of postcond_cmd according to type change

--- coq/proof/SoundPostcondCmd.v
+++ coq/proof/SoundPostcondCmd.v
@@ -67,22 +67,24 @@ Definition FORGET_MEMORY_TODO: Prop. Admitted. (* TODO *)
 
 (* TODO: we can assume source's progress *)
 Lemma postcond_cmd_forget_sound
-      conf_src st0_src st1_src def_src leaks_src def_memory_src
-      conf_tgt st0_tgt st1_tgt def_tgt leaks_tgt def_memory_tgt
+      conf_src st0_src st1_src def_src leaks_src cmd_src cmds_src
+      conf_tgt st0_tgt st1_tgt def_tgt leaks_tgt cmd_tgt cmds_tgt
       invst0 invmem0 inv0
       (STATE: InvState.Rel.sem conf_src conf_tgt st0_src st0_tgt invst0 invmem0 inv0)
       (MEM: InvMem.Rel.sem conf_src conf_tgt st0_src.(Mem) st0_tgt.(Mem) invmem0)
+      (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src)
+      (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt)
       (STATE_SRC: state_equiv_except def_src st0_src st1_src)
       (STATE_TGT: state_equiv_except def_tgt st0_tgt st1_tgt)
       (UNIQUE_SRC: unique_preserved_except conf_src invst0.(InvState.Rel.src) inv0.(Invariant.src) st1_src def_src leaks_src)
       (UNIQUE_TGT: unique_preserved_except conf_tgt invst0.(InvState.Rel.tgt) inv0.(Invariant.tgt) st1_tgt def_tgt leaks_tgt)
       (MEM_SRC: FORGET_MEMORY_TODO)
-      (MEM_TGT: FORGET_MEMORY_TODO):
-  exists invst1 invmem1,
+      (MEM_TGT: FORGET_MEMORY_TODO)
+  : exists invst1 invmem1,
     <<STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst1 invmem1
                               (Postcond.postcond_cmd_forget
+                                 cmd_src cmd_tgt
                                  def_src def_tgt leaks_src leaks_tgt
-                                 def_memory_src def_memory_tgt
                                  inv0)>> /\
     <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
     <<MEMLE: InvMem.Rel.le invmem0 invmem1>>.
