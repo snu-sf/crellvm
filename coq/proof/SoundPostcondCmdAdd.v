51d89a9d3c6798bd931327d9b823727efc932911
alxest <youngju.song@sf.snu.ac.kr>
2016-10-03 12:56:15 +0900

    Separate proof

--- coq/proof/SoundPostcondCmdAdd.v
+++ coq/proof/SoundPostcondCmdAdd.v
@@ -25,6 +25,7 @@ Require InvState.
 Require Import Inject.
 Require Import SoundBase.
 Require Import SoundForget.
+Require Import SoundReduceMaydiff.
 
 Set Implicit Arguments.
 
@@ -50,7 +51,73 @@ Proof.
   inv STEP; ss. inv CMDS. ss.
 Qed.
 
-Lemma postcond_cmd_add_sound
+Lemma postcond_cmd_add_private_unique_sound
+      conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
+      conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
+      invst0 invmem0 inv0
+      evt
+      (POSTCOND: Postcond.postcond_cmd_check
+                   cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt inv0)
+      (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0)
+      (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0)
+      (STEP_SRC: sInsn conf_src st0_src st1_src evt)
+      (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt)
+      (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src)
+      (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt)
+      (NONCALL_SRC: Instruction.isCallInst cmd_src = false)
+      (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false)
+      (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src)))
+      (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt)))
+      (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src))
+      (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)):
+  exists invst1 invmem1,
+    <<STATE: InvState.Rel.sem
+               conf_src conf_tgt st1_src st1_tgt invst1 invmem1
+               (Postcond.postcond_cmd_add_private_unique cmd_src cmd_tgt inv0)>> /\
+    <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
+    <<MEMLE: InvMem.Rel.le invmem0 invmem1>> /\
+    <<POSTCOND: Postcond.postcond_cmd_check
+                  cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt
+                  (Postcond.postcond_cmd_add_private_unique cmd_src cmd_tgt inv0)>>
+.
+Proof.
+Admitted.
+
+Lemma postcond_cmd_add_lessdef_src_sound
+      conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
+      conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
+      invst0 invmem0 inv0
+      evt
+      (POSTCOND: Postcond.postcond_cmd_check
+                   cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt inv0)
+      (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0)
+      (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0)
+      (STEP_SRC: sInsn conf_src st0_src st1_src evt)
+      (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt)
+      (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src)
+      (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt)
+      (NONCALL_SRC: Instruction.isCallInst cmd_src = false)
+      (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false)
+      (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src)))
+      (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt)))
+      (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src))
+      (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)):
+  exists invst1 invmem1,
+    <<STATE: InvState.Rel.sem
+               conf_src conf_tgt st1_src st1_tgt invst1 invmem1
+               (Invariant.update_src
+                  (Invariant.update_lessdef (postcond_cmd_add_lessdef cmd_src)) inv0)>> /\
+    <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
+    <<MEMLE: InvMem.Rel.le invmem0 invmem1>> /\
+    <<POSTCOND: Postcond.postcond_cmd_check
+                  cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt
+                  (Invariant.update_src
+                     (Invariant.update_lessdef (postcond_cmd_add_lessdef cmd_src)) inv0)>>
+.
+Proof.
+Admitted.
+
+Lemma postcond_cmd_add_lessdef_tgt_sound
       conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
       conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
       invst0 invmem0 inv0
@@ -69,9 +136,84 @@ Lemma postcond_cmd_add_sound
       (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src))
       (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)):
   exists invst1 invmem1,
+    <<STATE: InvState.Rel.sem
+               conf_src conf_tgt st1_src st1_tgt invst1 invmem1
+               (Invariant.update_tgt
+                  (Invariant.update_lessdef (postcond_cmd_add_lessdef cmd_tgt)) inv0)>> /\
+    <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
+    <<MEMLE: InvMem.Rel.le invmem0 invmem1>> /\
+    <<POSTCOND: Postcond.postcond_cmd_check
+                  cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt
+                  (Invariant.update_tgt
+                     (Invariant.update_lessdef (postcond_cmd_add_lessdef cmd_tgt)) inv0)>>
+.
+Proof.
+Admitted.
+
+Lemma postcond_cmd_add_remove_def_from_maydiff_sound
+      conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
+      conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
+      invst0 invmem0 inv0
+      evt
+      (POSTCOND: Postcond.postcond_cmd_check cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt inv0)
+      (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0)
+      (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0)
+      (STEP_SRC: sInsn conf_src st0_src st1_src evt)
+      (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt)
+      (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src)
+      (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt)
+      (NONCALL_SRC: Instruction.isCallInst cmd_src = false)
+      (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false)
+      (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src)))
+      (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt)))
+      (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src))
+      (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)):
+  exists invst1 invmem1,
+    <<STATE: InvState.Rel.sem
+               conf_src conf_tgt st1_src st1_tgt invst1 invmem1
+               (remove_def_from_maydiff cmd_src cmd_tgt inv0)>> /\
+    <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
+    <<MEMLE: InvMem.Rel.le invmem0 invmem1>> /\
+    <<POSTCOND: Postcond.postcond_cmd_check cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt
+                                            (remove_def_from_maydiff cmd_src cmd_tgt inv0)>>
+.
+Proof.
+Admitted.
+
+Theorem postcond_cmd_add_sound
+      m_src conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
+      m_tgt conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
+      invst0 invmem0 inv0
+      evt
+      (CONF: InvState.valid_conf m_src m_tgt conf_src conf_tgt)
+      (POSTCOND: Postcond.postcond_cmd_check cmd_src cmd_tgt
+                                             def_src def_tgt uses_src uses_tgt inv0)
+      (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0)
+      (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0)
+      (STEP_SRC: sInsn conf_src st0_src st1_src evt)
+      (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt)
+      (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src)
+      (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt)
+      (NONCALL_SRC: Instruction.isCallInst cmd_src = false)
+      (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false)
+      (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src)))
+      (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt)))
+      (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src))
+      (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)):
+  exists invst1 invmem1,
     <<STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst1 invmem1
                               (Postcond.postcond_cmd_add cmd_src cmd_tgt inv0)>> /\
     <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
     <<MEMLE: InvMem.Rel.le invmem0 invmem1>>.
 Proof.
-Admitted.
+  unfold postcond_cmd_add.
+  exploit postcond_cmd_add_private_unique_sound; eauto; []; ii; des.
+  (* guard. *)
+  exploit postcond_cmd_add_lessdef_src_sound; try apply STATE0; eauto; []; ii; des.
+  exploit postcond_cmd_add_lessdef_tgt_sound; try apply STATE1; eauto; []; ii; des.
+  exploit postcond_cmd_add_remove_def_from_maydiff_sound; try apply STATE2;
+    eauto; []; ii; des.
+  exploit reduce_maydiff_sound; try apply STATE3; eauto; []; ii; des.
+  esplits; eauto.
+  do 3 (eapply InvMem.Rel.PreOrder_le_obligation_2; eauto).
+Qed.
