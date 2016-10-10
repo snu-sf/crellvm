97d72d1a057e38b43837be6ca1200138f8783f55
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2016-10-10 16:20:57 +0900

    Merge add_private_unique and remove_maydiff

--- coq/proof/SoundPostcondCmdAdd.v
+++ coq/proof/SoundPostcondCmdAdd.v
@@ -52,7 +52,8 @@ Proof.
   inv STEP; ss. inv CMDS. ss.
 Qed.
 
-Lemma postcond_cmd_add_private_unique_sound
+(* TODO: let's ignore insn_malloc for now.  Revise the validator so that it rejects any occurence of insn_malloc. *)
+Lemma postcond_cmd_add_inject_sound
       conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
       conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
       invst0 invmem0 inv0
@@ -74,14 +75,33 @@ Lemma postcond_cmd_add_private_unique_sound
   exists invmem1,
     <<STATE: InvState.Rel.sem
                conf_src conf_tgt st1_src st1_tgt invst0 invmem1
-               (Postcond.postcond_cmd_add_private_unique cmd_src cmd_tgt inv0)>> /\
+               (Postcond.postcond_cmd_add_inject cmd_src cmd_tgt inv0)>> /\
     <<MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem1>> /\
     <<MEMLE: InvMem.Rel.le invmem0 invmem1>> /\
     <<POSTCOND: Postcond.postcond_cmd_check
                   cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt
-                  (Postcond.postcond_cmd_add_private_unique cmd_src cmd_tgt inv0)>>
+                  (Postcond.postcond_cmd_add_inject cmd_src cmd_tgt inv0)>>
 .
 Proof.
+  destruct (classic (Postcond.postcond_cmd_add_inject cmd_src cmd_tgt inv0 = inv0)).
+  { rewrite H in *. esplits; eauto. reflexivity. }
+  destruct cmd_src, cmd_tgt; ss.
+  - admit. (* alloca, nop *)
+    (* invmem1 = invmem0 *)
+    (* roughly, degenerate case for the last one *)
+  - admit. (* nop, allica *)
+  - (* alloca, alloca *)
+    (*
+     * invmem1 = invmem0 + (newl_src -> newl_tgt)
+     * invstate.rel.sem inv0: possible (monotone w.r.t. invmem)
+     * invstate.rel.sem unique (added): newl <> old locations
+         * newl: st0.mem.nextblock,
+           wf: st0's old locations < st0.mem.nextblock (genericvalues_inject.wf_sb_mi)
+     * invstate.rel.sem remove_def_from_maydiff (add): possible
+     * invmem.rel.sem: adding (newl_src -> newl_tgt)
+     * invmem.rel.le: possible
+     * postcond_cmd_check: monotonicity
+     *)
 Admitted.
 
 (* TODO Add To Metatheory?? *)
@@ -359,30 +379,30 @@ Proof.
     eapply postcond_cmd_add_lessdef_Subset.
 Qed.
 
-Lemma postcond_cmd_add_remove_def_from_maydiff_sound
-      conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
-      conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
-      invst0 invmem0 inv0
-      evt
-      (POSTCOND: Postcond.postcond_cmd_check cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt inv0)
-      (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0)
-      (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0)
-      (STEP_SRC: sInsn conf_src st0_src st1_src evt)
-      (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt)
-      (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src)
-      (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt)
-      (NONCALL_SRC: Instruction.isCallInst cmd_src = false)
-      (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false)
-      (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src)))
-      (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt)))
-      (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src))
-      (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)):
-    <<STATE: InvState.Rel.sem
-               conf_src conf_tgt st1_src st1_tgt invst0 invmem0
-               (remove_def_from_maydiff cmd_src cmd_tgt inv0)>>
-.
-Proof.
-Admitted.
+(* Lemma postcond_cmd_add_remove_def_from_maydiff_sound *)
+(*       conf_src st0_src st1_src cmd_src cmds_src def_src uses_src *)
+(*       conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt *)
+(*       invst0 invmem0 inv0 *)
+(*       evt *)
+(*       (POSTCOND: Postcond.postcond_cmd_check cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt inv0) *)
+(*       (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0) *)
+(*       (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0) *)
+(*       (STEP_SRC: sInsn conf_src st0_src st1_src evt) *)
+(*       (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt) *)
+(*       (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src) *)
+(*       (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt) *)
+(*       (NONCALL_SRC: Instruction.isCallInst cmd_src = false) *)
+(*       (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false) *)
+(*       (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src))) *)
+(*       (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt))) *)
+(*       (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src)) *)
+(*       (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)): *)
+(*     <<STATE: InvState.Rel.sem *)
+(*                conf_src conf_tgt st1_src st1_tgt invst0 invmem0 *)
+(*                (remove_def_from_maydiff cmd_src cmd_tgt inv0)>> *)
+(* . *)
+(* Proof. *)
+(* Admitted. *)
 
 Theorem postcond_cmd_add_sound
       m_src conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
@@ -411,11 +431,9 @@ Theorem postcond_cmd_add_sound
     <<MEMLE: InvMem.Rel.le invmem0 invmem1>>.
 Proof.
   unfold postcond_cmd_add.
-  exploit postcond_cmd_add_private_unique_sound; eauto; []; ii; des.
+  exploit postcond_cmd_add_inject_sound; eauto; []; ii; des.
   exploit postcond_cmd_add_lessdef_src_sound; try apply STATE0; eauto; []; ii; des.
   exploit postcond_cmd_add_lessdef_tgt_sound; try apply STATE1; eauto; []; ii; des.
-  exploit postcond_cmd_add_remove_def_from_maydiff_sound; try apply STATE2;
-    eauto; []; ii; des.
-  exploit reduce_maydiff_sound; try apply STATE3; eauto; []; ii; des.
+  exploit reduce_maydiff_sound; try apply STATE2; eauto; []; ii; des.
   esplits; eauto.
 Qed.
