a604b0cae50682ea02eb344fdb43573a1272cab5
alxest <youngju.song@sf.snu.ac.kr>
2016-11-01 16:13:55 +0900

    Cleanse some code && apply comment

--- coq/proof/SoundPostcondCmdAdd.v
+++ coq/proof/SoundPostcondCmdAdd.v
@@ -297,32 +297,6 @@ Unshelve.
 eauto.
 Qed.
 
-(* CALL FOR DISCUSSION *)
-(* Stronger premise, *)
-(* easier to prove, harder to use in theory *)
-(* However, actually it is more convenient to use, as we do not need to inv MEM *)
-
-(* Heard weird behavior of "apply" from @jhk, now I will state in fine-grained way.
-Added space in Axio m in order to avoid grep
-
-Axio m P: Prop.
-Axio m Q: Prop.
-Axio m R: Prop.
-Axio m S: Prop.
-Inductive PQ: Prop :=
-| PQ_intro: P -> Q -> PQ
-.
-Inductive RS: Prop :=
-| RS_intro: R -> S -> RS
-.
-Inductive PQRS: Prop :=
-| PQRS_intro:  PQ -> RS -> PQRS
-.
-
-Goal PQ -> P. ii. auto. eauto. apply H. Qed.
-Goal PQRS -> P. ii. auto. eauto. apply H. Qed.
- *)
-
 Lemma add_unique_malloc
   cmds_src id0 align0 inv_unary TD F B lc gn ECS0 tmn SRC_MEM als SRC_MEM_STEP tsz mb conf_src gmax
   (MALLOC: malloc TD SRC_MEM tsz gn align0 = Some (SRC_MEM_STEP, mb))
@@ -436,7 +410,6 @@ Proof.
   subst. ss.
 Qed.
 
-(* TODO: let's ignore insn_malloc for now.  Revise the validator so that it rejects any occurence of insn_malloc. *)
 Lemma postcond_cmd_add_inject_sound
       m_src conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
       m_tgt conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
@@ -1149,31 +1122,6 @@ Proof.
       splits; ss.
 Qed.
 
-(* Lemma postcond_cmd_add_remove_def_from_maydiff_sound *)
-(*       conf_src st0_src st1_src cmd_src cmds_src def_src uses_src *)
-(*       conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt *)
-(*       invst0 invmem0 inv0 *)
-(*       evt *)
-(*       (POSTCOND: Postcond.postcond_cmd_check cmd_src cmd_tgt def_src def_tgt uses_src uses_tgt inv0) *)
-(*       (STATE: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst0 invmem0 inv0) *)
-(*       (MEM: InvMem.Rel.sem conf_src conf_tgt st1_src.(Mem) st1_tgt.(Mem) invmem0) *)
-(*       (STEP_SRC: sInsn conf_src st0_src st1_src evt) *)
-(*       (STEP_TGT: sInsn conf_tgt st0_tgt st1_tgt evt) *)
-(*       (CMDS_SRC: st0_src.(EC).(CurCmds) = cmd_src :: cmds_src) *)
-(*       (CMDS_TGT: st0_tgt.(EC).(CurCmds) = cmd_tgt :: cmds_tgt) *)
-(*       (NONCALL_SRC: Instruction.isCallInst cmd_src = false) *)
-(*       (NONCALL_TGT: Instruction.isCallInst cmd_tgt = false) *)
-(*       (DEF_SRC: def_src = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_src))) *)
-(*       (DEF_TGT: def_tgt = AtomSetImpl_from_list (option_to_list (Cmd.get_def cmd_tgt))) *)
-(*       (USES_SRC: uses_src = AtomSetImpl_from_list (Cmd.get_ids cmd_src)) *)
-(*       (USES_TGT: uses_tgt = AtomSetImpl_from_list (Cmd.get_ids cmd_tgt)): *)
-(*     <<STATE: InvState.Rel.sem *)
-(*                conf_src conf_tgt st1_src st1_tgt invst0 invmem0 *)
-(*                (remove_def_from_maydiff cmd_src cmd_tgt inv0)>> *)
-(* . *)
-(* Proof. *)
-(* Admitted. *)
-
 Theorem postcond_cmd_add_sound
         m_src conf_src st0_src st1_src cmd_src cmds_src def_src uses_src
         m_tgt conf_tgt st0_tgt st1_tgt cmd_tgt cmds_tgt def_tgt uses_tgt
@@ -1201,8 +1149,6 @@ Theorem postcond_cmd_add_sound
                                     st1_src st1_tgt cmd_src cmd_tgt invmem1)
         (ALLOC_PRIVATE: alloc_private conf_src conf_tgt cmd_src cmd_tgt st0_src st0_tgt
                                       st1_src st1_tgt invmem1)
-        (* (ALLOC_INJECT: InvMem.Rel.inject invmem1 (st0_src.(Mem).(Memory.Mem.nextblock)) = *)
-        (*                Some((st0_tgt.(Mem).(Memory.Mem.nextblock)), 0)) *)
   :
   exists invst2 invmem2,
     <<STATE_STEP: InvState.Rel.sem conf_src conf_tgt st1_src st1_tgt invst2 invmem2
