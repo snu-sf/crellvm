3b73d92e9f9ae70be826a8cb7f2c6c90b7e89a9c
alxest <youngju.song@sf.snu.ac.kr>
2016-12-26 01:47:57 +0900

    Add count_admit script

---
+++ coq/count_admit.sh
@@ -0,0 +1,3 @@
+ag -s admit --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=proof/SimulationValid.v --ignore=status.sh
+#-s means case sensitive
+#http://minimul.com/ignoring-files-with-ag-silver-searcher.html
