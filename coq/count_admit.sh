98617a999c2f580c4b85eb3716b63d9751a48b12
alxest <youngju.song@sf.snu.ac.kr>
2017-01-29 15:20:46 +0900

    Update script

--- coq/count_admit.sh
+++ coq/count_admit.sh
@@ -1,8 +1,8 @@
-ag -s "Admitted" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Admitted" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
 echo "---------------------------------------------------------------------------------"
-ag -s "admit" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "admit" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
 echo "---------------------------------------------------------------------------------"
-ag -s "EXCUSED_ADMIT" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "EXCUSED_ADMIT" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
 #-s means case sensitive
 #http://minimul.com/ignoring-files-with-ag-silver-searcher.html
 
