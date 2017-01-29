455ff6fb6d543adbc04daa92c9043b982695b218
alxest <youngju.song@sf.snu.ac.kr>
2017-01-29 15:58:10 +0900

    Update count_admit

--- coq/count_admit.sh
+++ coq/count_admit.sh
@@ -2,7 +2,7 @@ ag -G v$ -s "Admitted" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v 
 echo "---------------------------------------------------------------------------------"
 ag -G v$ -s "admit" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
 echo "---------------------------------------------------------------------------------"
-ag -G v$ -s "EXCUSED_ADMIT" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "SF_ADMIT" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
 #-s means case sensitive
 #http://minimul.com/ignoring-files-with-ag-silver-searcher.html
 
