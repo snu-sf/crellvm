e61724963f7634158a89fcdc5d3e00ba3e95d70d
alxest <youngju.song@sf.snu.ac.kr>
2017-01-29 16:05:28 +0900

    Update count_admit.sh

--- coq/count_admit.sh
+++ coq/count_admit.sh
@@ -1,9 +1,29 @@
+echo "----------------------------------------------------------------------------------------------------------"
+echo "------------------------------Admitted--------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
 ag -G v$ -s "Admitted" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
-echo "---------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
+echo "------------------------------admit-----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
 ag -G v$ -s "admit" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
-echo "---------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
+echo "------------------------------SF_ADMIT--------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
 ag -G v$ -s "SF_ADMIT" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
 #-s means case sensitive
 #http://minimul.com/ignoring-files-with-ag-silver-searcher.html
 
 #TODO move all proof in "def" to "proof", and only grep "proof".
+echo "----------------------------------------------------------------------------------------------------------"
+echo "------------------------------Other assumption keywords---------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------"
+#https://coq.inria.fr/refman/Reference-Manual003.html#Vernacular
+#assumption_keyword	::=	Axiom | Conjecture |	Parameter | Parameters |	Variable | Variables |	Hypothesis | Hypotheses
+ag -G v$ -s "Axiom" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Conjecture" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Parameter" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Parameters" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Variable" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Variables" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Hypothesis" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
+ag -G v$ -s "Hypotheses" --ignore=proof/Adequacy.v --ignore=proof/AdequacyLocal.v --ignore=proof/SimulationNop.v --ignore=status.sh --ignore=count_admit.sh
