5406806a453c569dc19d1e57ccf1713b84585e2b
Yoonseung Kim <yoonseung.kim@sf.snu.ac.kr>
2017-02-20 15:26:07 +0900

    Fix typo in infruleGen

--- ocaml/infruleGen.ml
+++ ocaml/infruleGen.ml
@@ -5,7 +5,8 @@ open Syntax
     LLVMsyntax.cmd -> LLVMsyntax.cmd -> Invariant.t -> Infrule.t list **)
 
 let gen_infrules_from_cmds
-      (cmd_src cmd_tgt:LLVMsyntax.cmd)
+      (cmd_src : LLVMsyntax.cmd)
+      (cmd_tgt : LLVMsyntax.cmd)
       (inv : Invariant.t)
     : Infrule.t list
   = []
@@ -13,6 +14,7 @@ let gen_infrules_from_cmds
 (** val gen_infrules : Invariant.t -> Invariant.t -> Infrule.t list **)
 
 let gen_infrules
-      (inv inv_nxt : Invariant.t)
+      (inv  : Invariant.t)
+      (inv_nxt : Invariant.t)
     : Infrule.t list
   = []
